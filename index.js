66d7b16513cf10706102564305e533c0b7d1d9bc
David N <paperish@icloud.com>
2025-10-16 13:41:37 -0600

    Finished rest/spread syntax lab

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: undefined
+  };
 
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+};
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
